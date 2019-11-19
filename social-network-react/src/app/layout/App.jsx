96b8cbc626cf3a2893d455ec26cea5f91b2a926b
carolgmonteiro <carolgmonteiro@live.com>
2019-11-19 13:46:32 -0300

    eventList and EventAttendee in eventDasboard are with props

--- social-network-react/src/app/layout/App.jsx
+++ social-network-react/src/app/layout/App.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+// import { Button } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Invents</h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
